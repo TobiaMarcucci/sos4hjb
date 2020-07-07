ed496db37e49bb994735ba4526e508ef813904f3
Tobia Marcucci <tobia.marcucci@gmail.com>
2020-07-07 19:12:16 +0200

    Updates tests for Variable class.

--- sos4hjb/test/polynomials/test_variable.py
+++ sos4hjb/test/polynomials/test_variable.py
@@ -4,78 +4,47 @@ from sos4hjb.polynomials import Variable
 
 class TestVariable(unittest.TestCase):
 
-    def test_init_and_repr(self):
+    def test_init(self):
 
-        # Correct initializations.
         x = Variable('x')
-        self.assertEqual(x.__repr__(), 'x')
-        x = Variable('x', None)
-        self.assertEqual(x.__repr__(), 'x')
-        x0 = Variable('x', 0)
-        self.assertEqual(x0.__repr__(), 'x_{0}')
+        self.assertEqual(x.name, 'x')
+        self.assertEqual(x.index, 0)
 
-        # Wrong initializations.
-        with self.assertRaises(ValueError):
-            Variable(0)
-        with self.assertRaises(ValueError):
-            Variable('x', 'a')
-        with self.assertRaises(ValueError):
-            Variable('x', -1)
-
-    def test_orderings(self):
-
-        # Different name.
-        a = Variable('a')
-        b = Variable('b')
-        self._test_all_orderings(a, b)
-        aa = Variable('aa')
-        self._test_all_orderings(a, aa)
-
-        # Different index.
-        x0 = Variable('x', 0)
         x1 = Variable('x', 1)
-        self._test_all_orderings(x0, x1)
-        x3 = Variable('x', 3)
-        x11 = Variable('x', 11)
-        self._test_all_orderings(x3, x11)
+        self.assertEqual(x1.name, 'x')
+        self.assertEqual(x1.index, 1)
 
-        # Different index and name.
-        blue3 = Variable('blue', 3)
-        red0 = Variable('red', 0)
-        self._test_all_orderings(blue3, red0)
+    def test_eq_ineq(self):
 
-        # With index vs without index.
-        x = Variable('x')
-        x0 = Variable('x', 0)
-        self._test_all_orderings(x, x0)
-
-        # Equality between different instances.
         x = Variable('x')
         y = Variable('x')
         self.assertTrue(x == y)
-        x = Variable('x', 0)
-        y = Variable('x', 0)
-        self.assertTrue(x == y)
+        self.assertFalse(x != y)
+        y = Variable('y')
+        self.assertFalse(x == y)
+        self.assertTrue(x != y)
 
-    def _test_all_orderings(self, a, b):
+        x1 = Variable('x', 1)
+        x2 = Variable('x', 1)
+        self.assertTrue(x1 == x2)
+        self.assertFalse(x1 != x2)
+        x2 = Variable('x', 2)
+        self.assertFalse(x1 == x2)
+        self.assertTrue(x1 != x2)
 
-        # Test all the methods for a < b.
-        self.assertTrue(a < b)
-        self.assertTrue(a <= b)
-        self.assertTrue(b > a)
-        self.assertTrue(b >= a)
-        self.assertTrue(a != b)
-        self.assertTrue(a == a)
-        self.assertFalse(b < a)
-        self.assertFalse(b <= a)
-        self.assertFalse(a > b)
-        self.assertFalse(a >= b)
-        self.assertFalse(a != a)
-        self.assertFalse(a == b)
+    def test_repr(self):
+
+        x = Variable('x')
+        self.assertEqual(x.__repr__(), 'x')
+
+        x1 = Variable('x', 1)
+        self.assertEqual(x1.__repr__(), 'x_{1}')
+
+        x11 = Variable('x', 11)
+        self.assertEqual(x11.__repr__(), 'x_{11}')
 
     def test_multivariate(self):
 
-        # Correct initializations.
-        x = Variable.multivariate('x', 3)
+        x = Variable.multivariate('x', 5)
         for i, xi in enumerate(x):
-            self.assertTrue(xi.__repr__() == f'x_{{{i}}}')
+            self.assertTrue(xi.__repr__() == f'x_{{{i+1}}}')
